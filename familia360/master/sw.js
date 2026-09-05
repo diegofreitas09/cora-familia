58b47892b50448a14dd215d4cd60301d6a46c310
diegofreitas09 <diegofreitas090707@gmail.com>
2026-09-05 17:29:50 -0300

    feat(familia360): add master PWA service worker

---
+++ familia360/master/sw.js
@@ -0,0 +1,4 @@
+const CACHE='familia360-master-v1';
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./manifest.webmanifest'])).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./',copy));return response}).catch(()=>caches.match('./')))}});
